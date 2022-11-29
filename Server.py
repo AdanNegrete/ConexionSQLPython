57e0587de44a3f5305ecb4b3aba92bcd2223fd67
Adan Nega <shiryu200199@hotmail.com>
2022-11-28 23:40:04 -0600

    Updt : header.html & index.html

--- Server.py
+++ Server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import pyodbc
 
 Server = Flask(__name__)
