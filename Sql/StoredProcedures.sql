8bc9a03fd783780c38e99f9c637d191e30ce6da0
RodrigoCastillo <94335514+RoyCast3@users.noreply.github.com>
2022-11-30 00:56:20 -0600

    Update StoredProcedures.sql

--- Sql/StoredProcedures.sql
+++ Sql/StoredProcedures.sql
@@ -7,3 +7,34 @@ BEGIN TRAN
     EXEC sys.[sp_executesql] @SQL
 COMMIT TRAN
 END
+
+
+/**********************************************************************************************/
+--A
+--Creacion del sp para realizar la consulta remota
+create procedure ATotalProductos (@cat int) as
+begin
+	select soh.TerritoryID, sum(a.LineTotal) as VentasTotales
+	from AdventureWorks2019.Sales.SalesOrderHeader soh
+	inner join
+	(select salesorderid, productid, orderqty, linetotal
+	from AdventureWorks2019.Sales.salesorderdetail sod
+	where ProductID in (
+			select productid
+			from AdventureWorks2019.Production.Product
+			where ProductSubcategoryID in (
+				select ProductSubcategoryID
+				from AdventureWorks2019.Production.ProductSubcategory
+				where ProductCategoryID in (
+					select ProductCategoryID
+					from AdventureWorks2019.Production.ProductCategory
+					where ProductCategoryID = @cat 
+					)))) as a
+	on soh.SalesOrderID = a.SalesOrderID
+	group by soh.TerritoryID
+	order by soh.TerritoryID
+end
+go
+--Ejecucion del SP
+exec ATotalProductos 1
+go
