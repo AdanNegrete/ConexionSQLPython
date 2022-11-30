7c4c0cacb04fa3db3294c06851e3383bcce8bac3
Adan Nega <shiryu200199@hotmail.com>
2022-11-30 04:14:47 -0600

    End : consulta_a

--- routes/consultas.py
+++ routes/consultas.py
@@ -12,6 +12,19 @@ instancias={
     'other': 'NEGA-PC'
 }
 
+def complete_SelCat():
+    global instancias
+    global inst
+    categories = []
+    conn = connection(inst)
+    cursor = conn.cursor()
+    cursor.execute("EXEC dbo.usp_CategoryList ?",instancias.get('production'))
+    print(cursor)
+    for row in cursor.fetchall():
+        categories.append({"id": row[0], "name": row[1]})
+    conn.close()
+    return categories
+
 @consultas.route("/") #For default route
 def Index():
     return render_template("index.html")
@@ -49,16 +62,7 @@ def consulta():
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 @consultas.route("/consulta_a")
 def consulta_a():
-    global instancias
-    global inst
-    categories = []
-    conn = connection(inst)
-    cursor = conn.cursor()
-    cursor.execute("EXEC dbo.usp_CategoryList ?",instancias.get('production'))
-    print(cursor)
-    for row in cursor.fetchall():
-        categories.append({"id": row[0], "name": row[1]})
-    conn.close()
+    categories = complete_SelCat()
     return render_template('consulta_a.html', categories = categories)
 
 @consultas.route("/consatrr", methods=['POST'])
@@ -78,8 +82,8 @@ def consatrr():
         for row in cursor.fetchall():
             ventas.append({"TerrName": row[0], "VentasTotales": row[1]})
         conn.close()
-
-        return redirect(url_for('consultas.consulta_a'))
+        categories = complete_SelCat()
+        return render_template('consulta_a.html', ventas = ventas, categories = categories)
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 @consultas.route("/consulta_b")
 def consulta_b():
